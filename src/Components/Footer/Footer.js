42f5d3ada1c15a9a8bedfdf75111520478fd520b
mayur <mayuragrawal2194@gmail.com>
2023-09-18 00:52:39 +0530

    Testing

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
           <div className="clear" />
           <div className={`${FooterStyles.m7r2} text-white mt-5`}>
             Copyright ©2020 All rights reserved | This template is made with <span className={FooterStyles.heart}>♡</span> by
-            <span> Colorlib</span>
+            <span> Colorlib MAYUR AGARWAL</span>
           </div>
         </div>
       </div>
